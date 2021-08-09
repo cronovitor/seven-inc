3ee4b2cd57fac627402a4329420f045fac60f2d6
cronovitor <81451219+cronovitor@users.noreply.github.com>
2021-08-09 14:30:15 -0300

    update

--- src/employees.ts
+++ src/employees.ts
@@ -1,26 +1,17 @@
 import * as mongoose from "mongoose";
 
-// const uri: string = "mongodb://127.0.0.1:27017/local";
-
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://cronovitor:<0708Cronovitor>@cluster0.uebb7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 //const uri: string = "mongodb://127.0.0.1:27017/local"
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect((err: any) => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("employees").collection("devices");
   // perform actions on the collection object
   client.close();
 });
 
-// mongoose.connect(uri, (err: any) => {
-//   if (err) {
-//     console.log(err.message);
-//   } else {
-//     console.log("Successfully Connected!");
-//   }
-// });
 
-export interface employees extends mongoose.Document {
+export interface Iemployees extends mongoose.Document {
   id: Number;
   name: String;
   bornDate: Date;
@@ -39,8 +30,7 @@ export const employeesSchema = new mongoose.Schema({
 
 });
 
-const employeModel = 
 
-const employees = mongoose.model('employees', employeesSchema); 
+const employees = mongoose.model<Iemployees>('employees', employeesSchema); 
 
 export default employees;
